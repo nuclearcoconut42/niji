0d77d0516d1615f8d95c3d91121f99278395441c
Elliot Brock <nuclearcoconut86@gmail.com>
2016-09-06 21:03:37 -0400

    split up routes

--- niji/routes/api/suite.js
+++ niji/routes/api/suite.js
@@ -1,45 +1,8 @@
 var express = require('express');
-var multer = require('multer');
 
-var Theme = require.main.require('./models/theme');
 var Suite = require.main.require('./models/suite');
 
 var router = express.Router();
-
-router.post('/theme', function(req, res, next){
-  Theme.find({name: req.body.name}, function(err, doc){
-    if(err){
-      return res.status(404).json({
-        title: 'An error occurred',
-        error: err
-      });
-    }
-    if(doc){
-      return res.status(409).json({
-        message: 'A theme with that name already exists'
-      });
-    }
-  });
-  var theme = new Theme({
-    name: req.body.name,
-    description: req.body.description,
-    target: req.body.target,
-    tags: req.body.tags,
-    yaml: req.body.yaml
-  });
-  theme.save(function(err, result){
-    if(err){
-      return res.status(404).json({
-        title: 'An error occurred',
-        error: err
-      });
-    }
-    res.status(201).json({
-      message: 'Saved theme',
-      obj: result
-    });
-  });
-});
 
 router.post('/suite', function(req, res, next){
   Suite.find({name: req.body.name}, function(err, doc){
